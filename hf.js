89222656f9d39e0db526b41aa4a109870661b4bf
69imi420 <klimesimi2002@gmail.com>
2024-10-13 02:19:22 +0200

    pilot

---
+++ hf.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+       
+        document.querySelectorAll('p.highlight').forEach(p => {
+            p.classList.remove('highlight');
+        });
+
+      
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        const p = targetSection.querySelector('p');
+        if (p) {
+            p.classList.add('highlight');
+        }
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+   
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
